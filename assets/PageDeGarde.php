2d2fb8eb064ad7320a9a36fee915f2ce29defb34
Aurelie <aubin200683@gmail.com>
2025-10-24 18:56:09 +0200

    stage

---
+++ assets/PageDeGarde.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Binet Aurélie
+    Epreuve E5
+
+    Stage du 13 octobre au 15 novembre 2025
+    chez Mr Lambert Psychologue
+</body>
+</html>
