f5c00fc4e5374855273dd35589d730aecb3f3d15
Aurelie <aubin200683@gmail.com>
2025-10-24 18:16:39 +0200

    Portfolio

--- description e5.php
+++ description e5.php
@@ -8,15 +8,15 @@ include 'includes/header.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Idendité</title>
-    Je m'apelle Aurélie Binet.
+    
+</head>
+<body>
+  Je m'apelle Aurélie Binet.
     BTS SIO SLAM
 
     Description du travail de Mr LAMBERT :
     -Réaliser un site pour son cabinet
     -Réaliser un référencement
-    -Que le site soit .fr  
-</head>
-<body>
-    
+    -Que le site soit .fr    
 </body>
 </html>
