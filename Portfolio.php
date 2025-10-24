26a68cf15315f6a8372b65efd0c8da1fe8fe6b09
Aurelie <aubin200683@gmail.com>
2025-10-24 18:39:24 +0200

    Portfolio

--- Portfolio.php
+++ Portfolio.php
@@ -32,19 +32,10 @@ include 'includes/header.php';
         <li class="nav-item">
           <a class="nav-link" href="#">description de l'épreuve E5</a>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            E5 dans mon programme
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
+        <li class="nav-item" >
+          <a class="nav-link" href="#">stage</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">fin du stage</a>
+      
         </li>
       </ul>
       
